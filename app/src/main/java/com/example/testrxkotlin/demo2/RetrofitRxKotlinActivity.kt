f6a33be477f2bc269a38c9cfcc159854216773d2
chenzhi <chenzhi@qq.com>
2021-12-28 17:14:27 +0900

    添加retrofit与RxKotlin结合的例子

--- app/src/main/java/com/example/testrxkotlin/demo2/RetrofitRxKotlinActivity.kt
+++ app/src/main/java/com/example/testrxkotlin/demo2/RetrofitRxKotlinActivity.kt
@@ -2,15 +2,22 @@ package com.example.testrxkotlin.demo2
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.testrxkotlin.databinding.ActivityRetrofitRxKotlinBinding
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observer
 import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+
+
 
 class RetrofitRxKotlinActivity : AppCompatActivity() {
     val TAG = "RetrofitRxKotlinActivity"
@@ -27,9 +34,13 @@ class RetrofitRxKotlinActivity : AppCompatActivity() {
         setContentView(binding.root)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        test1()
 
+        binding.Button1.setOnClickListener { test1() }
         binding.Button2.setOnClickListener { test2() }
+        binding.Button3.setOnClickListener { test3() }
+        binding.Button4.setOnClickListener { test4() }
+        binding.Button5.setOnClickListener { test5() }
+
     }
 
     //简单创建。但是得到结果后，不能在前端实现
@@ -71,6 +82,23 @@ class RetrofitRxKotlinActivity : AppCompatActivity() {
     }
 
 
+    //得到结果后，在前端实现 扩展Observer
+    fun test3(){
+        val retrofit = Retrofit.Builder()
+            //.client(build.build())
+            .baseUrl("http://zipcloud.ibsnet.co.jp/")
+            .addConverterFactory(GsonConverterFactory.create()) //把json转为gson，才可以直接用LiveData.postValue
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.createWithScheduler(Schedulers.io()))
+            .build()
+
+        val service = retrofit.create(ApiClient::class.java)
+        service.getZipCode("0790177")
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(observer)
+    }
+
+
     val observer: Observer<Any> = object : Observer<Any> {
         override fun onComplete() {
             Log.e(TAG, ("All Completed"))
@@ -79,7 +107,6 @@ class RetrofitRxKotlinActivity : AppCompatActivity() {
 
         override fun onNext(item: Any) {
             Log.e(TAG, ("Next $item"))
-
 
             binding.result.text=item.toString()
         }
@@ -91,5 +118,69 @@ class RetrofitRxKotlinActivity : AppCompatActivity() {
         override fun onSubscribe(d: Disposable) {
             Log.e(TAG, ("New Subscription "))
         }
+    }
+
+
+    fun test4(){
+        //连接器设置的创建
+        val build = OkHttpClient.Builder()
+            .connectTimeout(60,TimeUnit.SECONDS)    //连接超时  SECONDS 秒
+            .writeTimeout(60,TimeUnit.SECONDS)      //写超时   SECONDS 秒
+            .readTimeout(60,TimeUnit.SECONDS)       //读取超时  SECONDS 秒
+
+        val retrofit = Retrofit.Builder()
+            .client(build.build())                              //连接器设置的配置
+            .baseUrl("http://zipcloud.ibsnet.co.jp/")
+            .addConverterFactory(GsonConverterFactory.create()) //把json转为gson，才可以直接用LiveData.postValue
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.createWithScheduler(Schedulers.io()))
+            .build()
+
+        val service = retrofit.create(ApiClient::class.java)
+        service.getZipCode("0790177")
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnNext {
+                Log.d(TAG, "response=$it")
+                binding.result.text = it.toString()
+            }
+            .doOnError {
+            }
+            .subscribe()
+    }
+
+
+    fun test5(){
+        val build = OkHttpClient.Builder()
+            .connectTimeout(60,TimeUnit.SECONDS)    //连接超时  SECONDS 秒
+            .writeTimeout(60,TimeUnit.SECONDS)      //写超时   SECONDS 秒
+            .readTimeout(60,TimeUnit.SECONDS)       //读取超时  SECONDS 秒
+
+        //接收log管理器
+        val logging = HttpLoggingInterceptor {
+            Log.e("OkHttp logging :", it)
+        }
+        logging.level = HttpLoggingInterceptor.Level.BODY
+
+        //把log管理器，加入到OkHttpClient.Builder()中
+        build.addInterceptor(logging)
+
+        val retrofit = Retrofit.Builder()
+            .client(build.build())                              //连接器设置的配置
+            .baseUrl("http://zipcloud.ibsnet.co.jp/")
+            .addConverterFactory(GsonConverterFactory.create()) //把json转为gson，才可以直接用LiveData.postValue
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.createWithScheduler(Schedulers.io()))
+            .build()
+
+        val service = retrofit.create(ApiClient::class.java)
+        service.getZipCode("0790177")
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnNext {
+                Toast.makeText(this, "能够在logcat中，看到服务器连接的log", Toast.LENGTH_SHORT).show()
+                binding.result.text = it.toString()
+            }
+            .doOnError {
+            }
+            .subscribe()
     }
 }
